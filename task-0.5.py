ece80877856281a64df9e947b17cd9350df21c2e
Mpumelelo Kenneth Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-10-15 11:31:18 +0200

    Task 0.5

--- task-0.5.py
+++ task-0.5.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 def area_of_triangle(s1, s2, s3):
+    ''' Calculates the Area of any triangle.
+    '''
     area = 0.5 * (s1 + s2 + s3)
     print(f"Area of the triangle is {area} sq units")
     
