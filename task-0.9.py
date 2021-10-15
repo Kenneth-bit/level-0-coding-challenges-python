12c29e1d9d9d0d22e19fc45374d9e44c7880c532
Mpumelelo Kenneth Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-10-15 17:51:26 +0200

    Task 0.9

--- task-0.9.py
+++ task-0.9.py
@@ -7,5 +7,8 @@ def vowels(string):
     for letter in string:
         if letter in vowel_list and letter not in output:
             output.append(letter.lower())
-    
+            
+    print("Vowels = ", end='')
     print(*output, sep=', ')
+
+vowels("Umuzi")
