415ff7716e32aaac85483eeee7f9874112a477d4
Mpumelelo Kenneth Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-10-15 17:21:51 +0200

    Task 0.9

---
+++ task-0.9.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+def vowels(string):
+    ''' Prints out all the vowels in the string
+    '''
+    output = []
+    vowel_list = 'aeiouAEIOU'
+    for letter in string:
+        if letter in vowel_list and letter not in output:
+            output.append(letter.lower())
+    
+    print(*output, sep=', ')
